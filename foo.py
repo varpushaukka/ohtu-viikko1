8dedbfd27e4f02a376ed90f415bb7052d64c6cb9
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2014-03-11 12:08:45 +0200

    kaksi tiedostoa ja hakemistoa lisätty

---
+++ foo.py
@@ -0,0 +1,16 @@
+
+def anagrammiko(sana1, sana2): return sorted(sana1) == sorted(sana2)
+
+print anagrammiko('mikko', 'komik')
+print anagrammiko('pannu', 'panu')
+
+def fibo(n):
+ def fibo_in(n):
+  if n <= 1: return 0, n
+  ed, nyk = fibo_in(n - 1)
+  return nyk, ed+nyk
+ return fibo_in(n)[1]
+
+print fibo(47)
+ 
+
